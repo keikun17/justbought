c037c81175c9751d0e8335b1f40135195f9a23bf
keikun17 <keikun17@gmail.com>
2013-06-16 00:57:49 +0800

    Initial mockup.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,10 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @posts = []
+    5.times do 
+      @posts << PostPresenter.new
+    end
   end
+
 end
