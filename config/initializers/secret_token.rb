6055f44dc584eefbbabdafed1e12aa2da43ec4e2
keikun17 <keikun17@gmail.com>
2013-06-15 11:27:06 +0800

    secret should be a secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Justbought::Application.config.secret_key_base = '4a0ed945e7faef593146d4aaab3f2e553f4acaeab5c98f1152a77dd3b8f0d98926f29f989dbee1d3c949c4f6043a58fde45f47c9a53650d0e5ddcb5f51ed2864'
+if Rails.env.test? || Rails.env.development?
+  Justbought::Application.config.secret_key_base = '4a0ed945e7faef593146d4aaab3f2e553f4acaeab5c98f1152a77dd3b8f0d98926f29f989dbee1d3c949c4f6043a58fde45f47c9a53650d0e5ddcb5f51ed2864'
+else
+  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
+  Justbought::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
+
