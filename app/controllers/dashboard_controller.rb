139252fbf1a2447478f6df1103e5e44ea833d84e
Buddy Magsipoc <keikun17@gmail.com>
2013-06-18 18:52:16 +0800

    Dashboard + Post form.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,8 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @posts = []
-    5.times do 
-      @posts << PostPresenter.new
-    end
+    @post = Post.new
+    @posts = Post.all
   end
 
 end
