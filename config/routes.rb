9cd27b3516302e5155663361bedfd80a7abc33e4
Buddy Magsipoc <keikun17@gmail.com>
2013-06-18 18:52:48 +0800

    Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Justbought::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
+  resources :posts
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
