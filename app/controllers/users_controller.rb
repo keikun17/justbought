8e2110843117c98a352b88753ab6ef49c6219be3
Buddy Magsipoc <keikun17@gmail.com>
2013-06-29 00:03:53 +0900

    Refactor + posts partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.decorate
+    @posts = @user.posts.paginate(page: params[:page], per_page: 5).decorate
   end
 
   def follow
