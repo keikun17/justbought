ff8510a03c4d5144b9de6675ba4314f279b2443d
Buddy Magsipoc <keikun17@gmail.com>
2013-06-29 23:49:21 +0900

    Require auth.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def create
     if @post = current_user.posts.create(post_params.merge(currency: current_user.currency))
       redirect_to root_path, flash: { error: @post.errors.full_messages.to_sentence }
