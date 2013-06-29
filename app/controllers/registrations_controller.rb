603ae10657144d16ce98aca8eca8ad83571d147d
Buddy Magsipoc <keikun17@gmail.com>
2013-06-29 23:27:44 +0900

    Autofollow self.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, only: [:update, :create]
 
+  def create
+    super
+    current_user.follow(current_user)
+  end
+
   protected
 
   def permitted_params
     [:username, :email, :currency, :password, :current_password]
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(permitted_params) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(permitted_params) }
