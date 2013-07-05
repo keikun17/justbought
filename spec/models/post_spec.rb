07c41592335cb590992980e43b0a8569f520923d
Buddy Magsipoc <keikun17@gmail.com>
2013-07-05 21:37:33 +0800

    Post Spec.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Post do
+
+end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  raw_comment :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  currency    :string(255)      default("$")
+#
+
