65ab99ca20018d75c319148fbc73ab5707f1185e
Buddy Magsipoc <keikun17@gmail.com>
2013-08-13 01:26:16 +0800

    Fix SWW.

--- app/poro/tag_validator.rb
+++ app/poro/tag_validator.rb
@@ -6,20 +6,20 @@ class TagValidator < ActiveModel::Validator
   end
 
   def check_hashtags(taggable)
-    taggable.errors.add(:raw_comment, "Should have #something hashtagged") unless have_hashtags?
+    taggable.errors.add(:raw_comment, "Should have #something hashtagged") unless have_hashtags?(taggable)
   end
 
   def check_cashtags(taggable)
-    taggable.errors.add(:raw_comment, "Should have a $price tag") unless have_cashtags?
+    taggable.errors.add(:raw_comment, "Should have a $price tag") unless have_cashtags?(taggable)
   end
 
   private
 
-  def have_hashtags?
+  def have_hashtags?(taggable)
     !TagExtractor.new(taggable.raw_comment).extract_hashtags.empty?
   end
 
-  def have_cashtags?
+  def have_cashtags?(taggable)
     !TagExtractor.new(taggable.raw_comment).extract_cashtags.empty?
   end
 end
