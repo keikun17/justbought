a91a050e3ddd656c8fbcc042805f8a3ac06be57f
Buddy Magsipoc <keikun17@gmail.com>
2013-08-12 15:06:28 +0800

    Test markup with capybara.

--- spec/poro/tag_parser_spec.rb
+++ spec/poro/tag_parser_spec.rb
@@ -2,23 +2,31 @@ require 'spec_helper'
 
 describe TagParser do
 
-  let(:string) do
-    "Just bought a #blackwidow #mechanicalkeyboard, i think it's worth $400"
-  end
+  let(:string) { "Just bought a #blackwidow #mechanicalkeyboard, i think it's worth $100" }
+  let(:instance) { described_class.new(raw: string )}
 
   describe 'parse' do
-    subject(:tag_parser) { described_class.new(raw: string )}
 
-    after do
-      tag_parser.parse
-    end
     it "should convert hashtags to links" do
-      tag_parser.should_receive(:linkify_hashtags!)
+      instance.should_receive(:linkify_hashtags!)
+      instance.parse
     end
 
     it "should convert cashtags to labels" do
-      tag_parser.should_receive(:labelize_cashtags!)
+      instance.should_receive(:labelize_cashtags!)
+      instance.parse
     end
+  end
+
+  describe 'labelize_cashtags!' do
+    subject(:markup) { Capybara.string(instance.labelize_cashtags!) }
+    it { should have_css("span.label.label-success", string: '$100') }
+  end
+
+  describe 'labelize_hashtags!' do
+    subject(:markup) { Capybara.string(instance.linkify_hashtags!) }
+    it { should have_css('a', string: 'blackwidow') }
+    it { should have_css('a', string: 'mechanicalkeyboard') }
   end
 
 end
