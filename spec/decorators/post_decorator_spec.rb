5e20a2bbd7dc43e186eac73e1d40894b7b8d218e
Buddy Magsipoc <keikun17@gmail.com>
2013-08-13 16:31:03 +0800

    Clarity.

--- spec/decorators/post_decorator_spec.rb
+++ spec/decorators/post_decorator_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
 describe PostDecorator do
-  subject { described_class.new(Object.new) }
+  let(:decorator) { described_class.new(Object.new) }
 
   describe "parsed_comment" do
     let(:parsed_string) { String.new }
 
+    after do
+      decorator.parsed_comment
+    end
+
     it "outputs html_safe parsed comment" do
-      subject.stub_chain(:parser, :parse).and_return(parsed_string)
+      decorator.stub_chain(:parser, :parse).and_return(parsed_string)
       parsed_string.should_receive(:html_safe)
-      subject.parsed_comment
     end
 
   end
